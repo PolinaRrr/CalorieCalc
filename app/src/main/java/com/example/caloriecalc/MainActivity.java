66e0b091ea5053053198299a7b9cd68af4b2c0ae
PolinaRrr <poniapocalypsisa@gmail.com>
2022-01-18 20:49:53 +0300

    2

--- app/src/main/java/com/example/caloriecalc/MainActivity.java
+++ app/src/main/java/com/example/caloriecalc/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.caloriecalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void button_add_onClick(View view) {
+        setContentView(R.layout.layout_add);
+    }
+
+    public void button_list_onClick(View view) {
+        setContentView(R.layout.layout_list);
+    }
+
+    public void button_del_onClick(View view) {
+        setContentView(R.layout.layout_del);
+    }
+
+    public void button_exit_onClick(View view) {
+        System.out.close();
+    }
+
 }
